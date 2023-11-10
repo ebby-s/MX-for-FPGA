e33b7d307db4a70279bf0454d5f583247e2daa70
Ebby Samson <es1219@ee-beholder0.ee.ic.ac.uk>
2023-11-10 17:01:54 +0000

    Verified unsigned_max, updated diagram,

--- tb/unsigned_max_tb.sv
+++ tb/unsigned_max_tb.sv
@@ -1,88 +1,90 @@
-module unsigned_max_tb (
+module unsigned_max_tb #(
+    parameter width  = 8,
+    parameter length = 32
+)(
     input logic i_clk,
     input logic i_rst_n,
     input logic unsigned [width-1:0] i_exps [length]
 );
 
-    localparam width = 8;
-    localparam length = 32;
+    localparam max_tree_depth = $clog2(length);
 
     // DUT
-    logic [width-1:0] p1_e_max_comb;
-    logic [width-1:0] p1_e_max_dly_1;
-    logic [width-1:0] p1_e_max_dly_2;
-    logic [width-1:0] p1_e_max_dly_5;
+    logic [width-1:0] p1_e_max        [max_tree_depth+3];
+    logic [width-1:0] p1_e_max_o_flop [max_tree_depth+3];
 
-    unsigned_max #(
-        .width(width),
-        .length(length),
-        .pl_freq(8)
-    ) u0_exp_max (
-        .i_clk(i_clk),
-        .i_exps(i_exps),
-        .o_e_max(p1_e_max)
-    );
+    for(genvar i=1; i<max_tree_depth+3; i++) begin : dut_delay
 
-    unsigned_max #(
-        .width(width),
-        .length(length),
-        .pl_freq(4)
-    ) u1_exp_max (
-        .i_clk(i_clk),
-        .i_exps(i_exps),
-        .o_e_max(p1_e_max_dly_1)
-    );
+        unsigned_max #(
+            .width(width),
+            .length(length),
+            .pl_freq(i),
+            .flop_output(0)
+        ) u0_exp_max (
+            .i_clk(i_clk),
+            .i_exps(i_exps),
+            .o_e_max(p1_e_max[i])
+        );
 
-    unsigned_max #(
-        .width(width),
-        .length(length),
-        .pl_freq(2)
-    ) u2_exp_max (
-        .i_clk(i_clk),
-        .i_exps(i_exps),
-        .o_e_max(p1_e_max_dly_2)
-    );
+        unsigned_max #(
+            .width(width),
+            .length(length),
+            .pl_freq(i),
+            .flop_output(1)
+        ) u1_exp_max (
+            .i_clk(i_clk),
+            .i_exps(i_exps),
+            .o_e_max(p1_e_max_o_flop[i])
+        );
+    end
 
-    unsigned_max #(
-        .width(width),
-        .length(length),
-        .pl_freq(1)
-    ) u3_exp_max (
-        .i_clk(i_clk),
-        .i_exps(i_exps),
-        .o_e_max(p1_e_max_dly_5)
-    );
 
     // Reference
     logic unsigned [width-1:0] p0_ref_e_max;
 
     always_comb begin
-        ref_e_max = 0;
+        p0_ref_e_max = 0;
         for(int i=0; i<length; i++) begin
             p0_ref_e_max = p0_ref_e_max > i_exps[i] ? p0_ref_e_max : i_exps[i];
         end
     end
 
-    for(genvar i=0; i<5; i++) begin : ref_dly
-        logic [width-1:0] ref_e_max;
+    // Delay reference
+    logic [width-1:0] dly_ref_e_max [max_tree_depth+2];
 
-        always_ff @(posedge i_clk, negedge i_rst_n) begin
-            if(i != 0) begin
-                ref_e_max <= ref_dly[i-1].ref_e_max;
-            end else begin
-                ref_e_max <= p0_ref_e_max;
-            end
+    assign dly_ref_e_max[0] = p0_ref_e_max;
+
+    always_ff @(posedge i_clk) begin
+        for(int i=1; i<max_tree_depth+2; i++) begin
+            dly_ref_e_max[i] <= dly_ref_e_max[i-1];
         end
     end
 
 
-    // Properties
+    // Assert properties
     default clocking @(posedge i_clk); endclocking
     default disable iff (!i_rst_n);
 
-    ast_u_max_comb: assert property(
-        p1_e_max_comb == ref_e_max
-    );
+    for(genvar i=1; i<max_tree_depth+3; i++) begin : ast_delay
 
+        if(max_tree_depth/i > 0) begin
+            ast_u_max: assert property(
+                i_rst_n ##((max_tree_depth-1)/i) (p1_e_max[i] == dly_ref_e_max[(max_tree_depth-1)/i])
+            );
+
+            ast_u_max_o_flop: assert property(
+                i_rst_n ##((max_tree_depth-1)/i+1) (p1_e_max_o_flop[i] == dly_ref_e_max[((max_tree_depth-1)/i)+1])
+            );
+
+        end else begin
+            ast_u_max: assert property(
+                p1_e_max[i] == dly_ref_e_max[0]
+            );
+
+            ast_u_max_o_flop: assert property(
+                i_rst_n ##1 p1_e_max_o_flop[i] == dly_ref_e_max[1]
+            );
+        end
+    end
 
 endmodule
